27d8900c70e7a9a68eff1674109db0bf89007a80
JohnPaul6 <emo4lif1@gmail.com>
2016-03-17 18:37:07 +1100

    Part #1 - Player Menu

--- client/systems/playerMenu/dialog/player_settings.hpp
+++ client/systems/playerMenu/dialog/player_settings.hpp
@@ -6,32 +6,20 @@
 class playerSettings {
 
 	idd = playersys_DIALOG;
-	movingEnable = true;
+	movingEnable = false;
 	enableSimulation = true;
 	onLoad = "[] execVM 'client\systems\playerMenu\item_list.sqf'";
 
 	class controlsBackground {
 
-		class MainBG : IGUIBack {
+		class MainBG : w_RscPicture {
 			idc = -1;
-			colorText[] = {1, 1, 1, 1};
-			colorBackground[] = {0,0,0,0.6};
-
-			moving = true;
-			x = 0.0; y = 0.1;
-			w = .745; h = 0.65;
-		};
-
-		class TopBar: IGUIBack
-		{
-			idc = -1;
-			colorText[] = {1, 1, 1, 1};
-			colorBackground[] = {A3W_UICOLOR_R, A3W_UICOLOR_G, A3W_UICOLOR_B, 0.8};
-
-			x = 0;
-			y = 0.1;
-			w = 0.745;
-			h = 0.05;
+			text = "client\systems\playerMenu\dialog\img\SGC_Tab_1.paa";
+			moving = false;
+			x = -0.30; 
+			y = -0.25;
+			w = 1.3; 
+			h = 1.35;
 		};
 
 		class MainTitle : w_RscText {
@@ -39,28 +27,28 @@ class playerSettings {
 			text = "Player Inventory";
 			sizeEx = 0.04;
 			shadow = 2;
-			x = 0.260; y = 0.1;
+			x = 0.260; y = 0.1; //x = 0.260; y = 0.1;
 			w = 0.3; h = 0.05;
 		};
 
 		class waterIcon : w_RscPicture {
 			idc = -1;
 			text = "client\icons\water.paa";
-			x = 0.022; y = 0.2;
+			x = -0.15; y = 0.153; //x = -0.06; y = 0.153;
 			w = 0.04 / (4/3); h = 0.04;
 		};
 
 		class foodIcon : w_RscPicture {
 			idc = -1;
 			text = "client\icons\food.paa";
-			x = 0.022; y = 0.26;
+			x = -0.15; y = 0.22; //x = -0.06; y = 0.22;
 			w = 0.04 / (4/3); h = 0.04;
 		};
 
 		class moneyIcon : w_RscPicture {
 			idc = -1;
 			text = "client\icons\money.paa";
-			x = 0.022; y = 0.32;
+			x = -0.15; y = 0.28; //x = -0.06; y = 0.28;
 			w = 0.04 / (4/3); h = 0.04;
 		};
 
@@ -68,7 +56,7 @@ class playerSettings {
 			idc = water_text;
 			text = "";
 			sizeEx = 0.03;
-			x = 0.06; y = 0.193;
+			x = -0.11; y = 0.153; // x = -0.022; y = 0.153;
 			w = 0.3; h = 0.05;
 		};
 
@@ -76,7 +64,7 @@ class playerSettings {
 			idc = food_text;
 			sizeEx = 0.03;
 			text = "";
-			x = 0.06; y = 0.254;
+			x = -0.11; y = 0.22; //x = -0.022; y = 0.22;
 			w = 0.3; h = 0.05;
 		};
 
@@ -84,7 +72,7 @@ class playerSettings {
 			idc = money_text;
 			text = "";
 			sizeEx = 0.03;
-			x = 0.06; y = 0.313;
+			x = -0.11; y = 0.28; //x = -0.022; y = 0.28;
 			w = 0.3; h = 0.05;
 		};
 
@@ -92,7 +80,7 @@ class playerSettings {
 			idc = view_range_text;
 			text = "View range:";
 			sizeEx = 0.025;
-			x = 0.03; y = 0.40;
+			x = -0.13; y = 0.36; //x = -0.04; y = 0.36;
 			w = 0.3; h = 0.02;
 		};
 
@@ -116,6 +104,7 @@ class playerSettings {
 		class DropButton : w_RscButton {
 			idc = -1;
 			text = "Drop";
+			colorBackground[] = {0, 0, 0, 0};
 			onButtonClick = "[1] execVM 'client\systems\playerMenu\itemfnc.sqf'";
 			x = 0.610; y = 0.525;
 			w = 0.116; h = 0.033 * safezoneH;
@@ -124,6 +113,7 @@ class playerSettings {
 		class UseButton : w_RscButton {
 			idc = -1;
 			text = "Use";
+			colorBackground[] = {0, 0, 0, 0};
 			onButtonClick = "[0] execVM 'client\systems\playerMenu\itemfnc.sqf'";
 			x = 0.489; y = 0.525;
 			w = 0.116; h = 0.033 * safezoneH;
@@ -138,6 +128,7 @@ class playerSettings {
 		class DropcButton : w_RscButton {
 			idc = -1;
 			text = "Drop";
+			colorBackground[] = {0, 0, 0, 0};
 			onButtonClick = "[] execVM 'client\systems\playerMenu\dropMoney.sqf'";
 			x = 0.489; y = 0.60;
 			w = 0.116; h = 0.033 * safezoneH;
@@ -146,48 +137,82 @@ class playerSettings {
 		class CloseButton : w_RscButton {
 			idc = close_button;
 			text = "Close";
+			colorBackground[] = {0, 0, 0, 0};
 			onButtonClick = "[] execVM 'client\systems\playerMenu\closePlayerMenu.sqf'";
-			x = 0.02; y = 0.66;
+			x = -0.14; y = 0.70; //x = 0.02; y = 0.66; .138 differnce 
 			w = 0.125; h = 0.033 * safezoneH;
 		};
 
 		class GroupsButton : w_RscButton {
 			idc = groupButton;
 			text = "Group Management";
+			colorBackground[] = {0, 0, 0, 0};
 			onButtonClick = "[] execVM 'client\systems\groups\loadGroupManagement.sqf'";
-			x = 0.158; y = 0.66;
+			x = -0.005; y = 0.70; //x = 0.158; y = 0.66;
 			w = 0.225; h = 0.033 * safezoneH;
 		};
 
 		class btnDistanceNear : w_RscButton {
 			idc = -1;
 			text = "Near";
+			colorBackground[] = {0, 0, 0, 0};
 			onButtonClick = "setViewDistance 1100;";
-			x = 0.02; y = 0.43;
+			x = -0.14; y = 0.40; //x = 0.02; y = 0.43;
 			w = 0.125; h = 0.033 * safezoneH;
 		};
 
 		class btnDistanceMedium : w_RscButton {
 			idc = -1;
 			text = "Medium";
+			colorBackground[] = {0, 0, 0, 0};
 			onButtonClick = "setViewDistance 2200;";
-			x = 0.02; y = 0.5;
+			x = -0.14; y = 0.47; //x = 0.02; y = 0.5;
 			w = 0.125; h = 0.033 * safezoneH;
 		};
 
 		class btnDistanceFar : w_RscButton {
 			idc = -1;
 			text = "Far";
+			colorBackground[] = {0, 0, 0, 0};
 			onButtonClick = "setViewDistance 3300;";
-			x = 0.02; y = 0.57;
+			x = -0.14; y = 0.54; //x = 0.02; y = 0.57;
 			w = 0.125; h = 0.033 * safezoneH;
 		};
-
+		/*
 		class btnDistanceInsane : w_RscButton {
 			text = "Insane";
+			colorBackground[] = {0, 0, 0, 0};
 			onButtonClick = "setViewDistance 5000;";
 			x = 0.02; y = 0.60;
 			w = 0.125; h = 0.033 * safezoneH;
 		};
+		*/
+		class btnDistanceCustom : w_RscButton {
+			idc = -1;
+			text = "Custom";
+			colorBackground[] = {0, 0, 0, 0};
+			onButtonClick = "call CHVD_fnc_openDialog";
+			x = -0.14; y = 0.61;
+			w = 0.125; h = 0.033 * safezoneH;
+		};
+		
+		class Donatorbutton : w_RscButton {
+			idc = supporter_button;
+			text = "Supporter";
+			onButtonClick = "[] execVM 'client\systems\supporter\Donatorinit.sqf'";
+			colorBackground[] = {0, 0, 0, 0};
+			colorDisabled[] = {1,1,1,0.300000 };
+			x = -0.005; y = 0.61; //x = 0.158; y = 0.57;
+			w = 0.125; h = 0.033 * safezoneH;
+		};
+		/*
+		class btnJTS_PM : w_RscButton {
+			idc = -1;
+			text = "Messages";
+			onButtonClick = "[] execVM 'addons\JTS_PM\JTS_PM.sqf'";
+			x = 0.02; y = 0.57;
+			w = 0.125; h = 0.033 * safezoneH;
+		};
+		*/
 	};
 };
